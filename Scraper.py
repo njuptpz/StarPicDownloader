f505df4bcfeef4a0e0cde636bd30727283b03526
root <wuchaowei2015@gmail.com>
2019-10-14 15:34:38 +0800

    add baidu images

--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,11 @@
 import os
-from icrawler.builtin import BingImageCrawler
+from icrawler.builtin import BingImageCrawler,GoogleImageCrawler,BaiduImageCrawler
 import glob
 import socket
 
 starFilePath = "StarName.txt"
+
+idStarFilePath = "id_name_url.txt"
 
 
 def GetIpAddress():
@@ -24,25 +26,32 @@ def GetFileContent(fileName):
 
 
 def Download(storePath):
+    if not os.path.exists(storePath):
+        os.makedirs(storePath)
+
     ipAddr = GetIpAddress()
-    ipTag = int(ipAddr.split(".")[-1])-128
+    ipTag = int(ipAddr.split(".")[-1])-125
     print(ipTag)
-    lines = GetFileContent(starFilePath)
+    lines = GetFileContent(idStarFilePath)
     #print(len(lines))
     for i, line in enumerate(lines):
         if i%8 != ipTag:
             continue
-
-        name = line.strip()
-        file_path = os.path.join(storePath, name)
+        print(line)
+        cells = line.strip().split("\t")
+        print(cells)
+        starId = cells[0]
+        name = cells[1]
+        file_path = os.path.join(storePath, f"{starId}_{name}")
         if not os.path.exists(file_path):
             os.makedirs(file_path)
 
         bing_storage = {'root_dir': file_path}
-        bing_crawler = BingImageCrawler(parser_threads=4, downloader_threads=8, storage=bing_storage)
+        bing_crawler = BaiduImageCrawler(parser_threads=4, downloader_threads=8, storage=bing_storage)
         bing_crawler.crawl(keyword=name, max_num=100)
 
-basePath = "data"
+basePath = "/devdata/spider"
+#basePath = "data/spider"
 Download(basePath)
 # items = ListFiles(basePath)
 # print(len(items))
